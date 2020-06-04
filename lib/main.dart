7d5c5f823bf753ff564746bd024ab844a6b49593
Shoaib <shoaibswe@gmail.com>
2020-06-04 14:56:20 +0600

    search box added to phonebook

--- lib/main.dart
+++ lib/main.dart
@@ -14,18 +14,43 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   List dta;
+  List unFilteredData;
 
   Future<String> loadJsonData() async {
     var jsonText = await rootBundle.loadString("assets/data.json");
     setState(() {
       dta = json.decode(jsonText);
+      this.unFilteredData = dta;
     });
   }
 
   @override
   void initState() {
+    super.initState();
     this.loadJsonData();
   }
+
+  void searchData(str) {
+    var strExist = str.length > 0 ? true : false;
+    if (strExist) {
+      var filteredData = [];
+      for (var i = 0; i < unFilteredData.length; i++) {
+       String name =unFilteredData[i]["name"].toUpperCase();
+       String phone =unFilteredData[i]["phone"];
+        if ((name.contains(str.toUpperCase())) || phone.contains(str) ) {
+          filteredData.add(unFilteredData[i]);
+        }
+      }
+      setState(() {
+        this.dta = filteredData;
+      });
+    } else {
+      setState(() {
+        this.dta = this.unFilteredData;
+      });
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -33,21 +58,40 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text("Address Book"),
         ),
-        body: ListView.builder(
-          itemCount: dta.length,
-          itemBuilder: (BuildContext context, int i) {
-            return Column(
-              children: <Widget>[
-                ListTile(
-                  leading: CircleAvatar(
-                    child: Text(dta[i]["name"][0]),
-                  ),
-                  title: Text(dta[i]["name"]),
-                  subtitle: Text(dta[i]["phone"]),
-                )
-              ],
-            );
-          },
+        body: Column(
+          children: <Widget>[
+        Container(
+          child: TextField(
+            decoration: InputDecoration(
+                hintText: "Search Name or Number",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(33))),
+            onChanged: (String str) {
+              this.searchData(str);
+//              print(str);
+            },
+          ),
+padding: EdgeInsets.only(left: 5,right: 5,top: 5),
+          color: Colors.lightBlueAccent
+        ),
+            Expanded(
+                child: ListView.builder(
+              itemCount: dta.length,
+              itemBuilder: (BuildContext context, int i) {
+                return Column(
+                  children: <Widget>[
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Text(dta[i]["name"][0]),
+                      ),
+                      title: Text(dta[i]["name"]),
+                      subtitle: Text(dta[i]["phone"]),
+                    )
+                  ],
+                );
+              },
+            )),
+          ],
         ));
   }
 }
